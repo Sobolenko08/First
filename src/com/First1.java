7312cfeb130ea7997057ae030f6e8a83b284f5d4
Sobolenko08 <164240507+Sobolenko08@users.noreply.github.com>
2024-04-16 20:05:00 +0300

    Add files via upload

---
+++ src/com/First1.java
@@ -0,0 +1,11 @@
+package com;
+
+public class First1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sasha");
+		System.out.println("________\n______");
+	}
+
+}
---
+++ src/com/Second.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
+		System.out.println("E");
+
+	}
+
+}
